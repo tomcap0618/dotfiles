75f71ce59c167ce8c6fb90d619a9e6fa371730a7
Tom Capuano <thomas.p.capuano@gmail.com>
2019-02-12 22:53:50 -0500

    Updated install to include fzf.

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 #Create Symbolic Links from dotfiles Repo to Home Directory.
 ln -sf ~/dotfiles/Xresources ~/.Xresources
 xrdb -load ~/.Xresources
+
 ln -sf ~/dotfiles/dircolors ~/.dircolors
+
+cp -r ~/dotfiles/fzf ~/.fzf
+~/.fzf/install --key-bindings --completion --update-rc
 
 ln -sf ~/dotfiles/bashrc ~/.bashrc
 
